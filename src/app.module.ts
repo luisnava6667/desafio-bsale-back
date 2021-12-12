594e42c270d85fed3a82f04a980ba32ef3ca9ad0
Jhorman Rus <jhormanrus@gmail.com>
2021-12-11 19:48:24 -0500

    [add] typeorm and models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Category } from './models/category';
+import { Product } from './models/product';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.HOST,
+      port: 3306,
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: [
+        Product,
+        Category
+      ],
+      synchronize: false,
+      logging: ['query']
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/models/category.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Product } from "./product";
+
+@Entity()
+export class Category {
+
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  name: string
+
+  @OneToMany(type => Product, product => product.category)
+  product: Category[]
+  
+}
